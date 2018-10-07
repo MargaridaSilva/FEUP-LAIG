fbc81c546505635af75f067ac4c9d658021671ad
Margarida Silva <margarida.rpds@gmail.com>
2018-10-07 20:22:56 +0100

    [Removed] Cleaned textures

--- project/MySceneGraph.js
+++ project/MySceneGraph.js
@@ -532,7 +532,7 @@ class MySceneGraph {
 
         this.textureIds.push(textureId);
         this.textures[textureId] = new CGFappearance(this.scene);
-        this.textures[textureId].loadTexture("/LAIG_2018/project/scenes/images/" + file);
+        this.textures[textureId].loadTexture("scenes/images/" + file);
     }
 
     parseMaterials(materialsNode) {
