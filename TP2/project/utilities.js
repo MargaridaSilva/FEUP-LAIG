7fb782a79836d1435717854513f53ae0e1bde0b4
Cesar-Medeiros <cesar.mnm@gmail.com>
2018-11-24 18:48:30 +0000

    [Update] Utilities

--- TP2/project/utilities.js
+++ TP2/project/utilities.js
@@ -3,24 +3,6 @@ Array.prototype.peek = function () {
     return this[this.length - 1];
 }
 
-
-Array.prototype.normalize = function () {
-
-    var normSqr = 0;
-
-    for (let i = 0; i < this.length; i++) {
-        normSqr += this[i] * this[i];
-    }
-
-    let norm = Math.sqrt(normSqr);
-
-    for (let i = 0; i < this.length; i++) {
-        this[i] /= norm;
-    }
-
-    return this;
-}
-
 Array.prototype.merge = function (arr1, arr2) {
     var ret = [];
 
@@ -41,4 +23,67 @@ Array.prototype.merge = function (arr1, arr2) {
 isPowerOfTwo = function(x)
 {
     return (x & (x - 1)) == 0;
+}
+
+const SEC_TO_MSEC = 1000;
+const DEG_TO_RAD = Math.PI/180;
+
+Array.prototype.norm = function () {
+
+    var normSqr = 0;
+
+    for (let i = 0; i < this.length; i++) {
+        normSqr += this[i] * this[i];
+    }
+
+    let norm = Math.sqrt(normSqr);
+    return norm;
+}
+
+
+Array.prototype.minus = function (array2) {
+
+    if(this.size != array2.size) return undefined;
+
+    let x = this.map(function (item, index) {
+        return item - array2[index];
+    })
+    return x;
+}
+
+
+Array.prototype.add = function (array2) {
+
+    if(this.size != array2.size) return undefined;
+
+    let x = this.map(function (item, index) {
+        return item + array2[index];
+    })
+    return x;
+}
+
+
+Array.prototype.div = function (constant) {
+
+    let x = this.map(function (item) {
+        return item / constant;
+    })
+    return x;
+}
+
+Array.prototype.normalize = function () {
+
+    var normSqr = 0;
+
+    for (let i = 0; i < this.length; i++) {
+        normSqr += this[i] * this[i];
+    }
+
+    let norm = Math.sqrt(normSqr);
+
+    for (let i = 0; i < this.length; i++) {
+        this[i] /= norm;
+    }
+
+    return this;
 }
